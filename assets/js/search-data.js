be97410cc7bbdb724955b7b67f652119c2c9fc06
AmbreBouillant <ambre.bouillant@u-paris.fr>
2026-05-11 14:47:09 +0000

    Deploying to gh-pages from @ AmbreBouillant/ambrebouillant.github.io@7967b173b1294435077d27c1440b3251da7f87b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-ambre-bouillant",
-    title: "Ambre Bouillant",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
